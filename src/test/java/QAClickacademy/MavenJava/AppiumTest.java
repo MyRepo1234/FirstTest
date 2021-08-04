756f018ece87576e1619ba7c6557e40a26c0cfcf
unknown <mandala.mamatha96@gmail.com>
2021-08-04 09:35:45 +0530

    my project

---
+++ src/test/java/QAClickacademy/MavenJava/AppiumTest.java
@@ -0,0 +1,17 @@
+package QAClickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAppLogin() {
+		System.out.println("NativeAppLogin");
+	
+	}
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");
+	}
+
+}
---
+++ src/test/java/QAClickacademy/MavenJava/SeleniumTest.java
@@ -0,0 +1,17 @@
+package QAClickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void browserWindow()
+	{
+		System.out.println("browserWindow");
+   }
+  @Test
+  public void windowLogin()
+  {
+	  System.out.println("windowLogin");
+  }
+  }
